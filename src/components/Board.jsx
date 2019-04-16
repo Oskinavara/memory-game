42269a154ae9c6cc3480cd6b3185ca1f9997ef3b
Oskinavara <oskinavara@gmail.com>
2019-04-16 22:35:37 +0200

    Add tiles, bottom bar, icons

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,26 +1,76 @@
 import React, { Component } from "react";
 
-
 class Board extends Component {
   state = {};
   render() {
     return (
       <div className="board">
-        <div className="container">
-          <div className="row">
-            <div className="col">.</div>
-            <div className="col">.</div>
-            <div className="col">.</div>
+        <div className="container ">
+          <div className="row h-50 ">
+            <div className="col">
+               <button className="tile">
+                <i className="fas fa-arrows-alt" />
+              </button>
+            </div>
+            <div className="col">
+               <button className="tile">
+                <i className="fas fa-arrows-alt" />
+              </button>
+            </div>
+            <div className="col">
+               <button className="tile">
+                <i className="fas fa-arrows-alt" />
+              </button>
+            </div>
+            <div className="col">
+               <button className="tile">
+                <i className="fas fa-arrows-alt" />
+              </button>
+            </div>
           </div>
-          <div className="row">
-            <div className="col">.</div>
-            <div className="col">.</div>
-            <div className="col">.</div>
+          <div className="row h-50">
+            <div className="col">
+               <button className="tile">
+                <i className="fas fa-arrows-alt" />
+              </button>
+            </div>
+            <div className="col">
+               <button className="tile">
+                <i className="fas fa-arrows-alt" />
+              </button>
+            </div>
+            <div className="col">
+               <button className="tile">
+                <i className="fas fa-arrows-alt" />
+              </button>
+            </div>
+            <div className="col">
+               <button className="tile">
+                <i className="fas fa-arrows-alt" />
+              </button>
+            </div>
           </div>
-          <div className="row">
-            <div className="col">.</div>
-            <div className="col">.</div>
-            <div className="col">.</div>
+          <div className="row h-50">
+            <div className="col">
+               <button className="tile">
+                <i className="fas fa-arrows-alt" />
+              </button>
+            </div>
+            <div className="col">
+               <button className="tile">
+                <i className="fas fa-arrows-alt" />
+              </button>
+            </div>
+            <div className="col">
+               <button className="tile">
+                <i className="fas fa-arrows-alt" />
+              </button>
+            </div>
+            <div className="col">
+               <button className="tile">
+                <i className="fas fa-arrows-alt" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
