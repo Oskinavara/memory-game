ae619a8d63fb38849104befb1575332348bf788c
Oskinavara <oskinavara@gmail.com>
2019-04-16 23:27:50 +0200

    Add Button Component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-
+var array=["up", "down", "right", "left"];
 class Button extends Component {
   state = {};
+  
+  
   render() {
     return (
       <div>
         <button className="tile">
-          <i className="fas fa-arrows-alt" />
+          {/* <i className="fas fa-arrow-alt-circle-right" /> */}
+          <i className={"fas fa-arrow-alt-circle-".concat(array[Math.floor(Math.random()*4)])} />
+          
         </button>
       </div>
     );
