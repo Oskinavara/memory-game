60f620f06510558cbb2b953390dae32db3c752b1
Oskinavara <oskinavara@gmail.com>
2019-04-18 22:25:57 +0200

    Add serviceWorker file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+serviceWorker.register();
